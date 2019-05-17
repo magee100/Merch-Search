4df93f5858a471dac82e37039dd1e4f4a2eb3b56
Quan <Tran.Q90@gmail.com>
2019-05-17 11:18:51 -0700

    forgot the same thing on a different page

--- client/src/pages/FrontPage.js
+++ client/src/pages/FrontPage.js
@@ -1,6 +1,6 @@
 import React, { Component} from "react";
 import API from "../utils/API";
-import { Container } from "../components/Grid";
+import { Container } from "../components/Grid/grid";
 import { List, ListItem } from "../components/Lists/lists";
 import Shirt from "../components/Shirt-module/shirt-module";
 
