b01c3791f9d330a87476fb250b772bea9b2a0830
David <dykim61@gmail.com>
2019-05-17 10:37:55 -0700

    adding Magee

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+//
 app.use(session({secret: "merch keyboard", resave: true, saveUnitialized: true}));
 
 // Serve up static assets (usually on heroku)
