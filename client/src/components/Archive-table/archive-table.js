434aaeca017566fdef491f5a32e5ec496f860957
Quan <Tran.Q90@gmail.com>
2019-05-21 11:14:59 -0700

    renaming again

--- client/src/components/Archive-table/archive-table.js
+++ client/src/components/Archive-table/archive-table.js
@@ -56,7 +56,7 @@ const rows = [
     { id: 'artist', numeric: false, disablePadding: false, label: 'Artist' },
     { id: 'type', numeric: false, disablePadding: false, label: 'Type' },
     { id: 'desc', numeric: false, disablePadding: false, label: 'Description' },
-    { id: 'releaseinfo', numeric: false, disablePadding: false, label: 'Release Info' },
+    { id: 'release', numeric: false, disablePadding: false, label: 'Release Info' },
     { id: 'color', numeric: false, disablePadding: false, label: 'Color' },
     { id: 'brand', numeric: false, disablePadding: false, label: 'Brand' },
 ];
