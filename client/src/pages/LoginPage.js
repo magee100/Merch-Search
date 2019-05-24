214abfc4adf3ac4c488c642ce4e63395f33e60b0
Andrew Magee <magee100@gmail.com>
2019-05-23 19:43:27 -0700

    fixed up login in jumbotron

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -4,6 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import API from "../utils/API";
 import { Container } from "../components/Grid/grid";
 import Shirt from "../components/Shirt-module/shirt-module";
+import classNames from 'classnames';
+
 
 const styles = theme => ({
 p: {
@@ -16,16 +18,55 @@ h: {
     fontSize: 50,
     },
 
+loginInfo: {
+    fontSize: 60,
+    
+},
+
+loginInfo2: {
+    fontSize: 30,
+    // marginLeft: 100,
+    lineHeight: 2,
+    textAlign: 'center',
+    
+},
+
+jumbotron: {
+    marginTop: 100,
+    margin: "0 auto",
+    width: 800,
+    height: 600,
+},
+
 })
-export default class LoginPage extends Component {
+ class LoginPage extends Component {
 
     render() {
         const { classes } =this.props;
 
         return (
-            <div>
-                <h1>HELLO LOGIN</h1>
+            <div
+            className={classes.jumbotron}
+            >
+                <h1
+                className={classes.loginInfo}
+                >Welcome to the 
+                
+                 Merch Search!</h1>
+                 <div className={classes.loginInfo2}>
+                <h2
+                
+                >Click on the user button 
+                in <br/>the navbar and sign up 
+                to see <br/>the archive and add items!</h2>
+                </div>
+                <p>
+
+
+                </p>
             </div>
         );
     }
 };
+
+export default withStyles(styles)(LoginPage)
